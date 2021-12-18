d2b56a8d85df4f9342a80da30e2b44b9800852b8
Rcrwrate <46920034+Rcrwrate@users.noreply.github.com>
2021-12-18 14:09:43 +0800

    new ver

--- get.py
+++ get.py
@@ -1,22 +1,43 @@
 import requests
 import json
+from msg import log
+
+def change_url_base(url_id = 2):
+    global url
+    if url_id == 0:
+        url = "https://raw.githubusercontent.com/Rcrwrate/Stellaris/main/"
+    elif url_id == "1":
+        url = "https://cdn.jsdelivr.net/gh/Rcrwrate/Stellaris/"
+    else:
+        url = "https://dl.phantom-sea-limited.ltd/Rcrwrate/Stellaris/main/"
+    log("change_url_base = "+url)
+
 
 def get_json(href):
+    href = url + href
     session = requests.Session()
     session.trust_env = False
     r = session.get(href)
     r = json.loads(r.text)
+    log("main.json: "+str(r))
     return r
 
 
 def download_file(filename, href):
+    href = url + href
     session = requests.Session()
     session.trust_env = False
     r = session.get(href)
+    path = '.log'
+    if not os.path.exists(path):
+        os.makedirs(path)
     with open(filename, 'wb') as fn:
         fn.write(r.content)
     log(filename + "\tOK\n")
     return 0
 
+
 if __name__ == "__main__":
-    get_json(href="https://cdn.jsdelivr.net/gh/Rcrwrate/Stellaris/main.json")
+    change_url_base()
+    json = get_json(href="main.json")
+    print(json["lady"]["key"])
--- msg.py
+++ msg.py
@@ -1,6 +1,21 @@
-global version = 0.1
+import os
+import time
 
+global version
+version = 0.1
 
 def msg(key):
-    
     return version
+    
+def log(thing):
+    i = time.asctime( time.localtime(time.time()) )
+    path = '.log'
+    if not os.path.exists(path):
+        os.makedirs(path)
+    with open('.log\log.log', 'a') as fn:
+        fn.write('='*16+ i + '='*16 +'\n')
+        fn.write(str(thing)+"\n\n")
+        
+
+if __name__ == "__main__":
+    log("test")
