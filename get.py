d2b7d32a496e2998ff6dad91fe407d9b65acc898
Rcrwrate <46920034+Rcrwrate@users.noreply.github.com>
2021-12-18 13:27:12 +0800

    update new data

--- get.py
+++ get.py
@@ -1,12 +1,22 @@
 import requests
+import json
 
-def download_file(href):
+def get_json(href):
     session = requests.Session()
     session.trust_env = False
     r = session.get(href)
-    print(r.text)
+    r = json.loads(r.text)
+    return r
+
+
+def download_file(filename, href):
+    session = requests.Session()
+    session.trust_env = False
+    r = session.get(href)
+    with open(filename, 'wb') as fn:
+        fn.write(r.content)
+    log(filename + "\tOK\n")
     return 0
 
-
 if __name__ == "__main__":
-    download_file(href="https://cdn.jsdelivr.net/gh/Rcrwrate/Stellaris")
+    get_json(href="https://cdn.jsdelivr.net/gh/Rcrwrate/Stellaris/main.json")
---
+++ msg.py
@@ -0,0 +1,6 @@
+global version = 0.1
+
+
+def msg(key):
+    
+    return version
